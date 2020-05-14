65ca2532564ad380a20061613442a226a9d302cb
aadi2305 <vicpediayt@gmail.com>
2020-05-14 19:23:10 +0530

    some glitch fixed

--- Main.py
+++ Main.py
@@ -59,23 +59,29 @@ def show():
 	
 	screen.blit(bg1, (x_bg1, 0))
 	screen.blit(bg1, (x_bg2, 0))
+	screen.blit(bg1, (x_bg3, 0))
 	mario_walking()
 	pygame.display.update()
 
 def bg_loop():
 	global x_bg1
-	global x_bg2
+	global x_bg2 , x_bg3
 	if x_mario >= 960 or x_mario <= 180:
 		x_bg1 = x_bg1 + bg_add
 		x_bg2 = x_bg2 + bg_add
-	if x_bg1 <= -1280:
-		x_bg1 = 1280
-	if x_bg2 <=-1280:
-		x_bg2 = 1280
-	if x_bg2 >1280:
+		x_bg3 = x_bg3 + bg_add
+	if x_bg1 < -1280:
+		x_bg1 = 1280*2
+	if x_bg2 <-1280:
+		x_bg2 = 1280*2
+	if x_bg3 <-1280:
+		x_bg3 = 1280*2
+	if x_bg2 >1280*2:
 		x_bg2 = -1280
-	if x_bg1 >1280:
+	if x_bg1 >1280*2:
 		x_bg1 = -1280
+	if x_bg3 >1280*2:
+		x_bg3 = -1280
 def jump():
 	global x_mario, y_mario, jumpPoints, isJump, d
 	if isJump == True:
@@ -92,6 +98,7 @@ def jump():
 #all the variables
 x_bg1 = 0
 x_bg2 = 1280
+x_bg3 = 1280*2
 x_mario = 200
 y_mario = 425
 bg_add = 0
@@ -133,10 +140,6 @@ while running:
 				mario_add = 0
 				right = False
 				left= False
-
-				
-	
-	print(x_mario)
 	bg_loop()
 	show()
 	
