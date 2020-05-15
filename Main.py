01a197a88e52627e16c60943a493abb50915b1c0
aadi2305 <vicpediayt@gmail.com>
2020-05-15 21:21:27 +0530

    duh

--- Main.py
+++ Main.py
@@ -179,6 +179,7 @@ while running:
 				temprf = False
 			if event.key == pygame.K_UP:
 				isJump = True
+				jumpPoints = jumpHeight
 
 		if event.type == pygame.KEYUP:
 			if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
